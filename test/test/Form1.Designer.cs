0197fe0cba1f1838e4a521c249a3f19e07af6bfa
rainyday9988 <rainyday9988@gmail.com>
2018-08-27 15:51:33 +0800

    testlogin1

--- test/test/Form1.Designer.cs
+++ test/test/Form1.Designer.cs
@@ -28,57 +28,101 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.tbuser = new System.Windows.Forms.TextBox();
+            this.tbpassword = new System.Windows.Forms.TextBox();
+            this.btnlogin = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblmessage = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // tbuser
             // 
-            this.button1.Location = new System.Drawing.Point(342, 84);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.tbuser.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbuser.Location = new System.Drawing.Point(187, 77);
+            this.tbuser.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.tbuser.Name = "tbuser";
+            this.tbuser.Size = new System.Drawing.Size(164, 29);
+            this.tbuser.TabIndex = 0;
             // 
-            // button2
+            // tbpassword
             // 
-            this.button2.Location = new System.Drawing.Point(342, 163);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.tbpassword.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.tbpassword.Location = new System.Drawing.Point(187, 136);
+            this.tbpassword.Margin = new System.Windows.Forms.Padding(5);
+            this.tbpassword.Name = "tbpassword";
+            this.tbpassword.Size = new System.Drawing.Size(164, 29);
+            this.tbpassword.TabIndex = 1;
             // 
-            // button3
+            // btnlogin
             // 
-            this.button3.Location = new System.Drawing.Point(342, 236);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
+            this.btnlogin.Location = new System.Drawing.Point(187, 253);
+            this.btnlogin.Name = "btnlogin";
+            this.btnlogin.Size = new System.Drawing.Size(80, 31);
+            this.btnlogin.TabIndex = 2;
+            this.btnlogin.Text = "登入";
+            this.btnlogin.UseVisualStyleBackColor = true;
+            this.btnlogin.Click += new System.EventHandler(this.btnlogin_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 86);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "帳號：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(103, 145);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "密碼：";
+            // 
+            // lblmessage
+            // 
+            this.lblmessage.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.lblmessage.ForeColor = System.Drawing.Color.Red;
+            this.lblmessage.Location = new System.Drawing.Point(183, 189);
+            this.lblmessage.Name = "lblmessage";
+            this.lblmessage.Size = new System.Drawing.Size(168, 61);
+            this.lblmessage.TabIndex = 5;
+            this.lblmessage.Text = "xxx";
+            this.lblmessage.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(547, 350);
+            this.Controls.Add(this.lblmessage);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnlogin);
+            this.Controls.Add(this.tbpassword);
+            this.Controls.Add(this.tbuser);
+            this.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox tbuser;
+        private System.Windows.Forms.TextBox tbpassword;
+        private System.Windows.Forms.Button btnlogin;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblmessage;
     }
 }
 
--- test/test/Form1.cs
+++ test/test/Form1.cs
@@ -7,14 +7,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace test
 {
     public partial class Form1 : Form
     {
+        SqlConnectionStringBuilder scsb;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            scsb = new SqlConnectionStringBuilder();
+            scsb.DataSource = ".";
+            scsb.InitialCatalog = "mybot";
+            scsb.IntegratedSecurity = true;
+            lblmessage.Visible = false;
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+           
+          
+                string user = tbuser.Text;
+                string pass = tbpassword.Text;
+            if ((user == "") || (pass == ""))
+            {
+
+                lblmessage.Text = "請輸入帳號&&密碼";
+                lblmessage.Visible = true;
+                
+            }
+            else
+            {
+                SqlConnection con = new SqlConnection(scsb.ToString());
+                con.Open();
+                string sqlmsg = "select*from tStudent where tStudent_fUser= @searchuser and tStudent_fPass= @searchpass";
+
+                SqlCommand cmd = new SqlCommand(sqlmsg, con);
+                cmd.Parameters.AddWithValue("@searchuser", tbuser.Text);
+                cmd.Parameters.AddWithValue("@searchpass", tbpassword.Text);
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.Read())
+                {
+
+                    Form2 myform2 = new Form2();
+                    myform2.formstrmsg = string.Format($"{reader["tStudent_fName"]}");
+                    myform2.ShowDialog();
+                    tbuser.Clear();
+                    tbpassword.Clear();
+
+                }
+                else
+                {
+                    lblmessage.Text = "無此帳號，請確認是否輸入正確";
+                    lblmessage.Visible = true;
+                    tbpassword.Text = "";
+
+
+                }
+                reader.Close();
+                con.Close();
+
+
+
+
+
+            }
         }
     }
 }
